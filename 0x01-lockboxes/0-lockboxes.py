d2efdcad0c1539fa56360cc7253547a330087f21
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-01 14:15:49 +0100

    refactoring

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,28 +4,34 @@
 
 def canUnlockAll(boxes):
     """
-        A function that checks if all the boxes can be unlocked
+    A function that checks if all the boxes can be unlocked.
 
-            boxes: locked boxes that contain keys that may unlock other boxes
+    boxes: locked boxes that contain keys that may unlock other boxes
     """
     num_of_boxes = len(boxes)
     max_box_number = num_of_boxes - 1
 
-    # store useful unique keys in a set
+    # Store useful unique keys in a set
     keys = {0}
 
-    # search the opened box for useful keys
+    # Search the opened box for useful keys
     for key in boxes[0]:
         if key <= max_box_number:
             keys.add(key)
 
-    # unlock boxes
-    for i in range(1, num_of_boxes):
-        if i < len(keys):
-            key = list(keys)[i]
-        for k in boxes[key]:
-            if k <= max_box_number:
-                keys.add(k)
-        if len(keys) == max_box_number:
-            return True
-    return False
+    # Unlock boxes
+    while True:
+        new_keys = set()  # Separate set to track new keys in this iteration
+        for key in keys:
+            for k in boxes[key]:
+                if k <= max_box_number and k not in keys:
+                    new_keys.add(k)
+                    if len(keys) + len(new_keys) == num_of_boxes:
+                        return True
+
+        if not new_keys:
+            break
+
+        keys.update(new_keys)  # Update the keys set after this iteration
+
+    return len(keys) == num_of_boxes
