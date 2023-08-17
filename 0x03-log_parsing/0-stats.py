d94cb5903b32366f101c2b6a87584ae5f3f27a5a
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-18 00:48:00 +0100

    edit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,16 +2,19 @@
 """0-stats"""
 import sys
 
+
 status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 code_count = {code: 0 for code in status_codes}
 total_size = 0
 line_count = 0
+
 
 def print_stats():
     print("File size: {}".format(total_size))
     for code in sorted(code_count.keys()):
         if code_count[code] > 0:
             print("{}: {}".format(code, code_count[code]))
+
 
 try:
     for line in sys.stdin:
@@ -28,5 +31,3 @@ try:
 except KeyboardInterrupt:
     print_stats()
     raise
-finally:
-    print_stats()
