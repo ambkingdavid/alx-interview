70fe7f0e968aa50e59b24871844bb937c459638a
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-18 00:39:28 +0100

    edit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,32 +1,36 @@
 #!/usr/bin/python3
 """0-stats"""
-import sys
+import re
+from collections import defaultdict
 
 
 status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-code_count = {code: 0 for code in status_codes}
+code_count = defaultdict(int)
 total_size = 0
 line_count = 0
+
+pattern = r'^(?P<ip>\S+) - \[(?P<date>.+)\] "(?P<method>\S+) \
+  (?P<path>\S+) (?P<protocol>\S+)" (?P<status_code>\d+) \
+    (?P<file_size>\d+)$'
 
 try:
     for line in sys.stdin:
         line_count += 1
-        data = line.split()
-        if len(data) > 2:
-            status_code = data[-2]
-            file_size = data[-1]
-            if status_code.isdigit() and file_size.isdigit():
-                code_count[int(status_code)] += 1
-                total_size += int(file_size)
+        match = re.search(pattern, line)
+        if match:
+            status_code = int(match.group('status_code'))
+            file_size = int(match.group('file_size'))
+            code_count[status_code] += 1
+            total_size += file_size
         if line_count % 10 == 0:
             print("File size: {}".format(total_size))
             for code in sorted(code_count.keys()):
-                if code_count[code] > 0:
+                if code in status_codes:
                     print("{}: {}".format(code, code_count[code]))
 except KeyboardInterrupt:
     pass
 finally:
     print("File size: {}".format(total_size))
     for code in sorted(code_count.keys()):
-        if code_count[code] > 0:
+        if code in status_codes:
             print("{}: {}".format(code, code_count[code]))
