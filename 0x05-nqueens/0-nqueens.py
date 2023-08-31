48d28cc61509cc20256683fea9662ad79c7333f7
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-08-31 12:41:56 +0100

    add update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,54 +6,27 @@ A module
 import sys
 
 
-def is_safe(board, row, col, N):
-    """
-    Check the left side of this row
-    """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solve_nqueens(N):
-    """
-    Solve_nqueens mthod
-    """
-    board = [[0 for _ in range(N)] for _ in range(N)]
+def nqueens(n):
     solutions = []
+    def nqueens_helper(queens, row):
+        if row == n:
+            solutions.append(queens)
+            return
+        
+        for col in range(n):
+            if is_valid(queens, row, col):
+                nqueens_helper(queens + [[row, col]], row + 1)
 
-    def solve_util(col):
-        """
-        utility method
-        """
-        if col >= N:
-            solution = []
-            for i in range(N):
-                for j in range(N):
-                    if board[i][j] == 1:
-                        solution.append([i, j])
-            solutions.append(solution)
-            return True
+    def is_valid(queens, row, col):
+        for queen in queens:
+            if queen[1] == col:
+                return False
+            
+            if abs(queen[0] - row) == abs(queen[1] - col):
+                return False
+        return True
 
-        for i in range(N):
-            if is_safe(board, i, col, N):
-                board[i][col] = 1
-                solve_util(col + 1)
-                board[i][col] = 0
-
-    solve_util(0)
+    nqueens_helper([], 0)
     return solutions
 
 
@@ -75,7 +48,7 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
-    solutions = solve_nqueens(N)
+    solutions = nqueens(N)
 
     for solution in solutions:
         print(solution)
