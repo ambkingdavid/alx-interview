2a0594662525cc73a2549f5ea0141b50e2344b1d
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-10-04 18:27:16 +0100

    initial commit

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+prime game module
+"""
+
+def isWinner(x, nums):
+    """
+    a function that determines the winner of a prime game
+    """
+    def is_prime(num):
+        """
+        checks if a number is prime
+        """
+        if num < 2:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def primeNumbers(n):
+        """
+        gets the prime numbers in a list of consecutive numbers
+
+        returns the length of the list
+        """
+        primeList = []
+
+        for num in n:
+            if is_prime(num):
+                primeList.append(num)
+        return len(primeList)
+
+    def is_even(n):
+        """
+        checks if a number is odd
+        """
+        if n % 2:
+            return True
+        return False
+
+    maria = 0
+    ben = 0
+
+    for round in range(x):
+        roundList = [num for num in range(nums[round])]
+        if primeNumbers(roundList) == 0:
+            ben += 1
+        elif is_even(primeNumbers(roundList)):
+            ben += 1
+        else:
+            maria += 1
+    if maria > ben:
+        return "Maria"
+    elif ben > maria:
+        return "Ben"
+    return None
