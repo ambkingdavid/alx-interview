3403901ab33edf65c5734952d9729a905d12552b
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-10-30 16:34:26 +0100

    add task

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Island Perimeter
+"""
+
+def island_perimeter(grid: list) -> int:
+    """
+    Island Perimeter
+    Args:
+        grid: List of ints
+    Returns:
+        The perimeter
+    """
+    perimeter = 0
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    for row in range(num_rows):
+        for col in range(num_cols):
+            # Check if the current cell is land
+            if grid[row][col] == 1:
+                # Check the top neighbor
+                if row - 1 < 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                # Check the bottom neighbor
+                if row + 1 >= num_rows or grid[row + 1][col] == 0:
+                    perimeter += 1
+                # Check the left neighbor
+                if col - 1 < 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                # Check the right neighbor
+                if col + 1 >= num_cols or grid[row][col + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
