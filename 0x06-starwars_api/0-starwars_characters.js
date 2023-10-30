67ab2ec6b49d21e2822363fe66064fce5a296c65
ambkingdavid <kelvinwooldbridge@gmail.com>
2023-10-30 16:40:18 +0100

    add changes to task

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,35 @@
 #!/usr/bin/node
-
-// Write a script that prints all characters of a Star Wars movie:
-
-// The first argument is the Movie ID - example: 3 = “Return of the Jedi”
-// Display one character name by line
-// You must use the Star wars API
-// You must use the module request
-
 const req = require('request');
-const movieId = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/';
 
-req.get(`${url}/${movieId}`, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const res = JSON.parse(body);
-    const idan = res.characters;
-    for (let i = 0; i < idan.length; i++) {
-      const ohk = idan[i];
-      req.get(`${ohk}`, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          const newres = JSON.parse(body);
-          console.log(newres.name);
-        }
-      });
-    }
+function getRequest (url) {
+  return new Promise((resolve, reject) => {
+    req.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode !== 200) {
+        reject(new Error(`Request failed with status code ${response.statusCode}`));
+      } else {
+        resolve(JSON.parse(body));
+      }
+    });
+  });
+}
+
+async function fetchMovieAndCharacters (movieId) {
+  try {
+    const movieData = await getRequest(`https://swapi-api.alx-tools.com/api/films/${movieId}`);
+    const characterPromises = movieData.characters.map(characterUrl =>
+      getRequest(characterUrl).then(characterData => characterData.name)
+    );
+
+    const characterNames = await Promise.all(characterPromises);
+    characterNames.forEach(characterName => {
+      console.log(`${characterName}`);
+    });
+  } catch (error) {
+    console.error(`Error: ${error}`);
   }
-});
+}
+
+const movieId = process.argv[2];
+fetchMovieAndCharacters(movieId);
